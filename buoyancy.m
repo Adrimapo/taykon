98e209da045a655e72448556488bba940e75770e
SaulN99 <saulnavajas@gmail.com>
2023-12-12 18:32:42 +0100

    new physics code and graphs

--- buoyancy.m
+++ buoyancy.m
@@ -1,15 +1,46 @@
 % Constants
 g = 9.81;              % Acceleration due to gravity (m/s²)
 rho_water = 997;       % Density of water (kg/m³)
-mass_range = linspace(0.4, 0.5, 100); % Mass in kg range for the robot (400-500 grams)
 
-% Buoyancy force calculation (Archimede's Principle)
-buoyancy_force = rho_water * mass_range * g;
+% Dimensions of the robot shell
+length_box = 0.05;     % 5 cm 
+width_box = 0.17;      % 17 cm 
+height_box = 0.098;    % 9.8 cm 
 
-% Plotting
+% Calculate the volume of the shell
+total_volume = length_box * width_box * height_box;
+
+% Volume range for the shell (submerged volume between 0% and 50%)
+volume_percentage_range = linspace(0, 1, 100);
+volume_range = volume_percentage_range * total_volume;
+
+% Buoyancy force calculation (Archimedes' Principle)
+buoyancy_force = rho_water * g * volume_range;
+
+% Error range for the submerged volume due to robot arms (+-5%)
+error_percentage = 0.05;
+error_volume = max(0, error_percentage * total_volume); % Ensure non-negative error
+
+%% Plotting with error area
 figure;
-plot(mass_range, buoyancy_force, 'LineWidth', 2);
-xlabel('Robot Mass (kg)');
-ylabel('Buoyancy Force (N/m³)');
-title('Buoyancy vs Robot Mass (Archimedes Principle)');
+hold on;
+
+% Plot buoyancy force
+plot(volume_percentage_range * 100, buoyancy_force, 'LineWidth', 2);
+
+% Plot error area
+error_area_upper = buoyancy_force + rho_water * g * error_volume;
+error_area_lower = buoyancy_force - rho_water * g * error_volume;
+
+% Create a filled area between upper and lower error bounds
+fill([volume_percentage_range*100, fliplr(volume_percentage_range*100)], [error_area_upper, fliplr(error_area_lower)], 'b', 'FaceAlpha', 0.3, 'EdgeColor', 'none', 'DisplayName', 'Error Range');
+
+xlabel('Submerged Volume (%)');
+ylabel('Buoyancy Force (N)');
+title('Buoyancy Force vs Volume Submerged (Archimedes Principle)');
 grid on;
+
+ylim([0, max(buoyancy_force) + max(rho_water * g * error_volume)]);
+
+legend('Buoyancy Force','Error Range');
+hold off;
--- equilibrium_point.m
+++ equilibrium_point.m
@@ -1,13 +1,13 @@
 % Constants
-g = 9.81;              % Acceleration due to gravity (m/s^2)
+g = 9.81;              % Acceleration due to gravity (m/s²)
 mass = 0.5;            % Assume a constant mass for the robot (kg)
 
 % Density range for fresh water at different temperatures
 temperature_range = 0:5:30; % Temperature in degrees Celsius
-rho_freshwater = 997 * (1 - 0.00021 * (temperature_range - 20)); % Density of fresh water (kg/m^3)
+rho_freshwater = 997 * (1 - 0.00021 * (temperature_range - 20)); % Density of fresh water (kg/m³)
 
 % Density range for saltwater at different temperatures
-rho_saltwater = 1025 * (1 - 0.00023 * (temperature_range - 20)); % Density of saltwater (kg/m^3)
+rho_saltwater = 1025 * (1 - 0.00023 * (temperature_range - 20)); % Density of saltwater (kg/m³)
 
 % Buoyancy force calculation for fresh water and saltwater at different temperatures
 buoyancy_freshwater = rho_freshwater * mass * g;
